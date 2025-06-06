7b2ee3a63d705702d6908705906edf80f01c8b81
ryyyvo <38389871+ryyyvo@users.noreply.github.com>
2025-06-05 23:10:10 -0700

    Add vite build files static dir

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,10 +1,13 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 
-dotenv.config(); // Read the .env file in the current working directory, and load values into process.env.
+dotenv.config();
 const PORT = process.env.PORT || 3000;
+const STATIC_DIR = process.env.STATIC_DIR || "public";
 
 const app = express();
+
+app.use(express.static(STATIC_DIR));
 
 app.get("/hello", (req: Request, res: Response) => {
     res.send("Hello, World");
