c723789359b30d662fae9298133e250615c2c247
ryyyvo <38389871+ryyyvo@users.noreply.github.com>
2025-06-11 17:13:00 -0700

    Configure Vite request forwarding

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -5,4 +5,11 @@ import checker from "vite-plugin-checker";
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react(), checker({ typescript: true })],
+    server: {
+    proxy: {
+        // Forward all requests from localhost:5173/api/* to localhost:3000/api/*
+        "/api": "http://localhost:3000",
+        "/auth": "http://localhost:3000",
+        "/uploads": "http://localhost:3000"
+    }}
 });
